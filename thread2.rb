8c8e003136af3c56b7d17f98ffd37c61a0ca557d
mystblue <mysterious_blue_star@yahoo.co.jp>
2011-12-21 18:38:47 -0800

    スレッドのサンプル

---
+++ thread2.rb
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+require "thread"
+
+m = Mutex.new
+
+t1 = Thread.new("test1") do |param1|
+	10.times do |i|
+		m.lock
+		puts i.to_s + ":" + param1
+		m.unlock
+	end
+end
+
+t2 = Thread.new("test2") do |param2|
+	10.times do |i|
+		m.lock
+		puts i.to_s + ":" + param2
+		m.unlock
+	end
+end
+
+t3 = Thread.new("test3") do |param3|
+	10.times do |i|
+		m.lock
+		puts i.to_s + ":" + param3
+		m.unlock
+	end
+end
+
+t1.join
+t2.join
+t3.join
+
+puts "end."
